208074d62bd7a1617280b7ef334f3edbc17b30de
abdullah97-pro <abdullahahmadi019@gmail.com>
2024-02-20 06:59:21 +0430

    create paragraph using javascript

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ var title = document.createElement("h1");
 // insert value into tag
 title.textContent = "Heading 1";
 
+// create p tag
+var content = document.createElement("p");
+content.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eos pariatur iusto rerum deleniti quaerat corporis accusantium modi, beatae id possimus minus repudiandae vel quae placeat consequatur necessitatibus reiciendis quasi!";
+
 
 // append element into the container
 var container = document.getElementById("container");
+// append h1
 container.appendChild(title);
+// append p
+container.appendChild(content);
